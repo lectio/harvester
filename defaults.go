ac81b71f8bb509de77309dd48a10491a6e5ff2bf
Shahid Shah <shahid@shah.org>
2019-03-30 12:01:35 -0400

    Make defaults private

--- defaults.go
+++ defaults.go
@@ -6,15 +6,15 @@ import (
 	"regexp"
 )
 
-type IgnoreURLsRegExList []*regexp.Regexp
-type RemoveParamsFromURLsRegExList []*regexp.Regexp
+type ignoreURLsRegExList []*regexp.Regexp
+type removeParamsFromURLsRegExList []*regexp.Regexp
 
-var DefaultIgnoreURLsRegExList IgnoreURLsRegExList = []*regexp.Regexp{regexp.MustCompile(`^https://twitter.com/(.*?)/status/(.*)$`), regexp.MustCompile(`https://t.co`)}
-var DefaultCleanURLsRegExList RemoveParamsFromURLsRegExList = []*regexp.Regexp{regexp.MustCompile(`^utm_`)}
-var DefaultWebPrefixRegEx = regexp.MustCompile(`^www.`)                 // Removes "www." from start of source links
-var DefaultTopLevelDomainSuffixRegEx = regexp.MustCompile(`\.[^\.]+?$`) // Removes ".com" and other TLD suffixes from end of hostname
+var defaultIgnoreURLsRegExList ignoreURLsRegExList = []*regexp.Regexp{regexp.MustCompile(`^https://twitter.com/(.*?)/status/(.*)$`), regexp.MustCompile(`https://t.co`)}
+var defaultCleanURLsRegExList removeParamsFromURLsRegExList = []*regexp.Regexp{regexp.MustCompile(`^utm_`)}
+var defaultWebPrefixRegEx = regexp.MustCompile(`^www.`)                 // Removes "www." from start of source links
+var defaultTopLevelDomainSuffixRegEx = regexp.MustCompile(`\.[^\.]+?$`) // Removes ".com" and other TLD suffixes from end of hostname
 
-func (l IgnoreURLsRegExList) IgnoreDiscoveredResource(url *url.URL) (bool, string) {
+func (l ignoreURLsRegExList) IgnoreDiscoveredResource(url *url.URL) (bool, string) {
 	URLtext := url.String()
 	for _, regEx := range l {
 		if regEx.MatchString(URLtext) {
@@ -24,12 +24,12 @@ func (l IgnoreURLsRegExList) IgnoreDiscoveredResource(url *url.URL) (bool, strin
 	return false, ""
 }
 
-func (l RemoveParamsFromURLsRegExList) CleanDiscoveredResource(url *url.URL) bool {
+func (l removeParamsFromURLsRegExList) CleanDiscoveredResource(url *url.URL) bool {
 	// we try to clean all URLs, not specific ones
 	return true
 }
 
-func (l RemoveParamsFromURLsRegExList) RemoveQueryParamFromResource(paramName string) (bool, string) {
+func (l removeParamsFromURLsRegExList) RemoveQueryParamFromResource(paramName string) (bool, string) {
 	for _, regEx := range l {
 		if regEx.MatchString(paramName) {
 			return true, fmt.Sprintf("Matched cleaner rule `%s`", regEx.String())
@@ -41,11 +41,11 @@ func (l RemoveParamsFromURLsRegExList) RemoveQueryParamFromResource(paramName st
 
 // GetSimplifiedHostname returns the URL's hostname without 'www.' prefix
 func GetSimplifiedHostname(url *url.URL) string {
-	return DefaultWebPrefixRegEx.ReplaceAllString(url.Hostname(), "")
+	return defaultWebPrefixRegEx.ReplaceAllString(url.Hostname(), "")
 }
 
 // GetSimplifiedHostnameWithoutTLD returns the URL's hostname without 'www.' prefix and removes the top level domain suffix (.com, etc.)
 func GetSimplifiedHostnameWithoutTLD(url *url.URL) string {
 	simplified := GetSimplifiedHostname(url)
-	return DefaultTopLevelDomainSuffixRegEx.ReplaceAllString(simplified, "")
+	return defaultTopLevelDomainSuffixRegEx.ReplaceAllString(simplified, "")
 }
